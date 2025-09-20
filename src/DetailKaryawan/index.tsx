023187835f0bca029e1af2694d4ba2fb727248c4
ravialrasya <raflyalrasya5@gmail.com>
2025-09-20 18:30:55 +0700

    update

--- src/DetailKaryawan/index.tsx
+++ src/DetailKaryawan/index.tsx
@@ -3,24 +3,26 @@
 import { useState } from "react";
 import Header from "../components/layout/header";
 import Sidebar from "../components/layout/sidebar";
-import { FaUser } from "react-icons/fa6";
 
 
-interface Absensi {
+
+interface TimeReport {
   tanggal: string;
   waktu: string;
+  waktudua: string;
   lokasi: string;
+  typedata: string;
 }
 
 export default function EmployeeDetail() {
-  const [tab, setTab] = useState<"time" | "absensi">("absensi");
+  const [tab, setTab] = useState<"time" | "Time Report">("Time Report");
 
-  const absensiData: Absensi[] = [
-    { tanggal: "10 Sep 2024", waktu: "08:00 - 16:00", lokasi: "Office" },
-    { tanggal: "09 Sep 2024", waktu: "08:00 - 16:00", lokasi: "Office" },
-    { tanggal: "08 Sep 2024", waktu: "08:00 - 16:00", lokasi: "Dinas" },
-    { tanggal: "07 Sep 2024", waktu: "08:00 - 16:00", lokasi: "Office" },
-    { tanggal: "06 Sep 2024", waktu: "08:00 - 16:00", lokasi: "Dinas" },
+  const absensiData: TimeReport[] = [
+    { tanggal: "10 Sep 2024", waktu: "08:00", typedata: "-", waktudua: "16:00",lokasi: "Total jam kerja : 8 Jam" },
+    { tanggal: "09 Sep 2024", waktu: "08:00", typedata: "-", waktudua: "16:00",lokasi: "Total jam kerja : 8 Jam" },
+    { tanggal: "08 Sep 2024", waktu: "08:00", typedata: "-", waktudua: "16:00",lokasi: "Total jam kerja : 8 Jam" },
+    { tanggal: "07 Sep 2024", waktu: "08:00", typedata: "-", waktudua: "16:00",lokasi: "Total jam kerja : 8 Jam" },
+    { tanggal: "06 Sep 2024", waktu: "08:00", typedata: "-", waktudua: "16:00",lokasi: "Total jam kerja : 8 Jam" },
   ];
   
 
@@ -33,12 +35,12 @@ export default function EmployeeDetail() {
         <div className="bg-gray-100 p-10">
 
            
-      <h1 className="text-xl font-semibold mb-6">Detail Karyawan</h1>
+      <h1 className="text-xl font-semibold font-montserrat text-[#2F2F2F] mb-6">Detail Karyawan</h1>
 
       {/* Profile */}
       <div className="flex items-center gap-4 ">
-        <div className="w-16 h-16 flex items-center justify-center rounded-full bg-gray-200 ">
-          <span className="text-[#0E64D1] text-4xl"><FaUser /></span>
+        <div className="w-[67px] h-[67px] flex items-center justify-center rounded-full bg-[#1176BC1A] ">
+          <img src="/img/user (1) 1.png" alt="" width={33.5} height={33.5}/>
         </div>
         <div>
           <h2 className="font-bold text-lg">Sava Alya Andini</h2>
@@ -52,64 +54,94 @@ export default function EmployeeDetail() {
 
       {/* Cards */}
       <div className="grid grid-cols-2 md:grid-cols-5 gap-4 mt-6">
-        <div className="bg-white p-4 rounded-lg">
-          <p>Total Absensi</p>
-          <p className="text-2xl font-bold">20</p>
+        <div className="bg-white shadow rounded-2xl p-4 h-[14vh] w-[30vh] flex flex-cols items-center justify-between ">
+          <div>
+          <p className="text-[14px] text-[#928FA6] font-inter ">Total Absensi</p>
+          <p className="text-[20px] font-inter font-bold">20</p>
+          </div>
+          <div><img src="/img/Group.png" alt="" width={20} height={21.81}/></div>
         </div>
-        <div className="bg-white p-4 rounded-lg">
-          <p>Total Time Report</p>
-          <p className="text-2xl font-bold">15</p>
+        
+        <div className="bg-white shadow rounded-2xl p-4 h-[14vh] w-[30vh] flex flex-cols items-center justify-between ">
+          <div>
+          <p className="text-[14px] text-[#928FA6] font-inter ">Total Time <br />Report</p>
+          <p className="text-[20px] font-inter font-bold">15</p>
+          </div>
+          <div><img src="/img/bxs_report.png" alt="" width={24} height={24}/></div>
         </div>
-        <div className="bg-white p-4 rounded-lg">
-          <p>Gaji Pokok</p>
-          <p className="text-2xl font-bold">Rp 5,500,000</p>
+        <div className="bg-white shadow rounded-2xl p-4 h-[14vh] w-[30vh] flex flex-cols items-center justify-between ">
+          <div>
+          <p className="text-[14px] text-[#928FA6] font-inter ">Gaji Pokok</p>
+          <p className="text-[20px] font-inter font-bold">Rp 5,500,000</p>
+          </div>
+          <div><img src="/img/teenyicons_money-solid.png" alt="" width={24} height={24}/></div>
         </div>
-        <div className="bg-white p-4 rounded-lg">
-          <p>Total Lembur</p>
-          <p className="text-2xl font-bold">Rp 500,000</p>
+        <div className="bg-white shadow rounded-2xl p-4 h-[14vh] w-[30vh] flex flex-cols items-center justify-between ">
+          <div>
+          <p className="text-[14px] text-[#928FA6] font-inter ">Total Lembur</p>
+          <p className="text-[20px] font-inter font-bold">Rp 500,000</p>
+          </div>
+          <div><img src="/img/teenyicons_money-solid.png" alt="" width={24} height={24}/></div>
         </div>
-        <div className="bg-white p-4 rounded-lg">
-          <p>Total</p>
-          <p className="text-2xl font-bold">Rp 6,000,000</p>
+        <div className="bg-white shadow rounded-2xl p-4 h-[14vh] w-[30vh] flex flex-cols items-center justify-between ">
+          <div>
+          <p className="text-[14px] text-[#928FA6] font-inter ">Total </p>
+          <p className="text-[20px] font-inter font-bold">Rp 6,000,000</p>
+          </div>
+          <div><img src="/img/mdi_set-all.png" alt="" width={24} height={24}/></div>
         </div>
       </div>
 
       {/* Tabs */}
-      <div className="flex gap-6 border-b border-gray-700 mt-8">
+      <div className="flex gap-[10px]  mt-8 " >
+        <div className="flex space-x-[141.5px] ">
         <button
-          onClick={() => setTab("time")}
+          onClick={() => setTab("Time Report")}
           className={`pb-2 ${
-            tab === "time"
-              ? "border-b-2 border-blue-500 text-blue-500"
-              : "text-gray-400"
+            tab === "Time Report"
+              ? "border-b-[1px] border-[#0E64D1] text-[#0E64D1] font-inter font-bold text-[14px]  w-[271.5px] h-[25px]"
+              : "text-[#556376] text-[14px]"
           }`}
         >
           Time Report
         </button>
         <button
-          onClick={() => setTab("absensi")}
+          onClick={() => setTab("time")}
           className={`pb-2 ${
-            tab === "absensi"
-              ? "border-b-2 border-blue-500 text-blue-500"
-              : "text-gray-400"
+            tab === "time"
+              ? "border-b-[1px] border-[#0E64D1] text-[#0E64D1] font-inter font-bold text-[14px]  w-[271.5px] h-[25px]"
+              : "text-[#556376] text-[14px]"
           }`}
         >
           Absensi
         </button>
+        
+        </div>
+        <select className="justify end rounded-lg cursor-pointer w-[87px] h-[35px] outline-1 outline-[#0E64D1] -mt-1 ml-auto ">
+          <option className="text-[14px] font-inter text-[#928FA6]">Bulan</option>
+        </select>
       </div>
 
       {/* Absensi List */}
-      {tab === "absensi" && (
+      {tab === "Time Report" && (
         <div className="mt-4 space-y-4">
           {absensiData.map((item, index) => (
             <div
               key={index}
-              className="bg-gray-200 p-4 rounded-lg flex justify-between items-center"
+              className="bg-white p-4 rounded-lg flex justify-between items-center cursor-pointer hover:shadow-sm"
             >
               <div>
-                <p className="text-sm text-gray-400">{item.tanggal}</p>
-                <p className="text-blue-500 font-semibold">{item.waktu}</p>
-                <p className="text-gray-300">{item.lokasi}</p>
+              <div className="flex space-x-2 mb-2">
+                <p className="flex "><img src="/img/calendar-minus 1.png" alt="" width={20} height={20} className="mr-2" />{item.tanggal}</p>
+                <span className="flex font-bold space-x-2">
+                  (
+                <p className="text-blue-500 ">{item.waktu}</p>
+                <p>{item.typedata}</p>
+                <p className="text-red-500 ">{item.waktudua}</p>
+                  )
+                </span>
+              </div>
+              <p className="text-gray-300">{item.lokasi}</p>
               </div>
               <button className="text-gray-500">{">"}</button>
             </div>
--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,17 +1,19 @@
-import { Bell } from 'lucide-react'
-import { FaUser } from 'react-icons/fa6'
+
+
 
 function Header() {
     return (
         <div className='w-full bg-white shadow-sm'>
             <header className="py-3 px-6">
                 <div className="flex justify-end gap-4">
-                    <button className="relative bg-gray-200 rounded-lg py-3 px-3 cursor-pointer">
-                        <span className="absolute ml-2 -mt-2 bg-red-500 text-red-500 text-xs rounded-full w-2 h-2">.</span>
-                        <Bell className="w-5 h-5 text-gray-600 " />
+                    <div>
+                    <button className="relative bg-[#B5B5B51A] rounded-lg w-[48px] h-[48px] cursor-pointer items-center justify-center flex">
+                        <span className="absolute ml-6 -mt-7  bg-red-500 text-red-500 text-xs rounded-full w-2 h-2 ">.</span>
+                        <img src="/img/clarity_notification-line.png" alt="" width={24} height={24}/>
                     </button>
-                    <button className="rounded-full bg-gray-200 py-3 px-3 cursor-pointer">
-                        <FaUser className="relative text-blue-600  w-5 h-5" />
+                    </div>
+                    <button className="rounded-full bg-[#1176BC1A] w-[40px] h-[40px] cursor-pointer items-center justify-center flex">
+                        <img src="/img/user (1) 1.png" alt="" width={20} height={20} />
                     </button>
                     <div className="font-medium ">
                         <select className="rounded-lg outline-none py-2 cursor-pointer">
